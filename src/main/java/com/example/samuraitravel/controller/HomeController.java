a95319bb095fc6db5e90d1716140657768683125
24tomoki <n.tomoki1115@gmail.como>
2025-12-16 17:47:27 +0900

    ハッシュコード取得削除

--- src/main/java/com/example/samuraitravel/controller/HomeController.java
+++ src/main/java/com/example/samuraitravel/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.example.samuraitravel.controller;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,10 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 	@GetMapping("/")
 	public String index() {
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String rawPassword = "password"; // ログインに使いたいパスワード
-        String encodedPassword = encoder.encode(rawPassword);
-        System.out.println(encodedPassword); // これが新しいハッシュ値
 		return "index";
 	}
 
