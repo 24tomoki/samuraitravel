48e9f45afc86b0395ae942f5215e5e0055955b4c
24tomoki <n.tomoki1115@gmail.como>
2026-02-28 17:25:04 +0900

    サービスクラス作成 会員登録を可能にする

--- src/main/java/com/example/samuraitravel/form/SignupForm.java
+++ src/main/java/com/example/samuraitravel/form/SignupForm.java
@@ -2,6 +2,7 @@ package com.example.samuraitravel.form;
 
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
@@ -21,6 +22,10 @@ public class SignupForm {
 	
 	@NotBlank(message = "電話番号を入力してください。")
 	private String phoneNumber;
+	
+	@NotBlank(message = "メールアドレスを入力してください。")
+	@Email(message = "メールアドレスは正しい形式で入力してください。")
+	private String email;
 	
 	@NotBlank(message = "パスワードを入力してください。")
 	@Length(min = 8, message = "パスワードは8文字以上で入力してください。")
